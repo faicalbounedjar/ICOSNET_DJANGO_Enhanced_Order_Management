ea29f0ea16bd919566205ca37b5df72dd7f6d447
Bounedjar Faiçal <62178820+faicalbounedjar@users.noreply.github.com>
2023-11-15 23:27:27 +0100

    Update settings.py Debug True

--- assessement/settings.py
+++ assessement/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0bezg#ozmd6^l*cc_7k@p=di8d&q_dnjv!gd^&*dpij3#vt0^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
